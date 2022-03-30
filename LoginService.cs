400375bac815e923ea8ff8cf37e0cbe6a739adc2
Troels <31724037+tr0els@users.noreply.github.com>
2022-03-30 13:49:05 +0200

    Add LoginService skeleton

---
+++ LoginService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginComponent
+{
+    public class LoginService : ILoginService
+    {
+        public bool CreateLogin(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HashAndSalt Hashing(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReHashing(string password, string salt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateLogin(string email, string newPassword, string oldPassword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
