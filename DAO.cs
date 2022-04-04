590b5ef5044951f94321f67b1276af170b98c04a
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2022-04-04 10:07:38 +0200

    small fix

--- DAO.cs
+++ DAO.cs
@@ -66,7 +66,7 @@ namespace LoginComponent
                 return true;
             }
 
-            return true;
+            return false;
         }
 
         public string GetLoginPasswordSalt(string email)
